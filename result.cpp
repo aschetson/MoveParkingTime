285fe68f9e82b44d064e071b8c9d8d2c6dc84d40
Щетнев Александр Викторович <Shchetnev-A@gaz-is.ru>
2022-01-24 01:17:19 +0300

    fixed output format

--- result.cpp
+++ result.cpp
@@ -6,7 +6,7 @@
 std::ostream &operator<<(std::ostream &os, const Result &res)
 {
     os << "id: " << res.id << std::endl;
-    os << "время в пути: " << toHours(res.moved) << "h";
-    os << "время стоянки: " << toHours(res.parked) << "h" << std::endl;
+    os << "время в пути: " << toHours(res.moved) << "h" << std::endl;
+    os << "время стоянки: " << toHours(res.parked) << "h";
     return os;
 }
